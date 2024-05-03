8d258c511a0702c5445bb5014754f7db111799f1
johnsilverio <johnvitor2002@hotmail.com>
2024-05-03 12:05:23 -0300

    Resolvido: problema 1004

---
+++ Bee1004/Bee1004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BeecrowdProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, PROD;
+
+            valor1 = int.Parse(Console.ReadLine());
+            valor2 = int.Parse(Console.ReadLine());
+            
+            PROD = valor1 * valor2;
+
+            Console.WriteLine($"PROD = {PROD}");
+        }
+    }
+}
