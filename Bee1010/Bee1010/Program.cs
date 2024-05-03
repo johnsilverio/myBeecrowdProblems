03b3872fe0c5beb302fb5e4d62329d962ec6433e
johnsilverio <johnvitor2002@hotmail.com>
2024-05-03 15:31:36 -0300

    Resolvido: problema 1014

--- Bee1010/Bee1010/Program.cs
+++ Bee1010/Bee1010/Program.cs
@@ -7,12 +7,12 @@ namespace BeecrowdProblems
     {
         static void Main(string[] args)
         {
-            string[] peca1 = Console.ReadLine().Split(" ");
+            string[] peca1 = Console.ReadLine().Split(' ');
             int codigoPeca1 = int.Parse(peca1[0]);
             int numeroDePeca1 = int.Parse(peca1[1]);
             double valorDaPeca1 = double.Parse(peca1[2], CultureInfo.InvariantCulture);
 
-            string[] peca2 = Console.ReadLine().Split(" ");
+            string[] peca2 = Console.ReadLine().Split(' ');
             int codigoPeca2 = int.Parse(peca2[0]);
             int numeroDePeca2 = int.Parse(peca2[1]);
             double valorDaPeca2 = double.Parse(peca2[2], CultureInfo.InvariantCulture);
---
+++ Bee1014/Bee1014/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace BeecrowdProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double consumoMedio = A / B;
+
+            Console.WriteLine($"{consumoMedio.ToString("F3", CultureInfo.InvariantCulture)} km/1");
+        }
+    }
+}
