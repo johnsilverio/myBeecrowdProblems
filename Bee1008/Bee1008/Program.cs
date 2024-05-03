b116f68c8f543f5b21500c9143b31581fab371e5
johnsilverio <johnvitor2002@hotmail.com>
2024-05-03 14:31:05 -0300

    Resolvido: problema 1008

---
+++ Bee1008/Bee1008/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace BeecrowdProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeFuncionario, numeroDeHorasTrabalhadas;
+            double valorRecebidoHora, salario;
+            Console.WriteLine("Numero de cadastro:");
+            numeroDeFuncionario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero de horas trabalhadas:");
+            numeroDeHorasTrabalhadas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor recebido por horas:");
+            valorRecebidoHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salario = numeroDeHorasTrabalhadas * valorRecebidoHora;
+
+            Console.WriteLine($"NUMBER = {numeroDeFuncionario}");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
