bd15a835cd458d2855458250f5e7952d67219fee
johnsilverio <johnvitor2002@hotmail.com>
2024-05-03 12:16:53 -0300

    Resolvido: problema 1007

---
+++ Bee1007/Bee1007/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BeecrowdProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, diferenca;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+            diferenca = A * B - C * D;
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}
