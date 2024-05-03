954c91cf27a5fcc8592b4b15d896fccc0d2729e2
johnsilverio <johnvitor2002@hotmail.com>
2024-05-03 01:51:45 -0300

    Resolvido: problema 1001

--- Bee1001/Bee1001/Program.cs
+++ Bee1001/Bee1001/Program.cs
@@ -6,11 +6,14 @@ namespace BeecrowdProblems
     {
         static void Main(string[] args)
         {
-            int A, B;
-            A = int.Parse(Console.ReadLine());
-            B = int.Parse(Console.ReadLine());
+            // Declaração e leitura de dois números inteiros do console.
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            // Calcula a soma dos dois números.
             int X = A + B;
 
+            // Exibe o resultado da soma formatado.
             Console.WriteLine($"X = {X}");
         }
     }
