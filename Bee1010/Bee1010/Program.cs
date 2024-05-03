fc044dcf49b52b6ff7b215c581afbe28423fbbe4
johnsilverio <johnvitor2002@hotmail.com>
2024-05-03 15:12:12 -0300

    Resolvido: problema 1010

---
+++ Bee1010/Bee1010/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace BeecrowdProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peca1 = Console.ReadLine().Split(" ");
+            int codigoPeca1 = int.Parse(peca1[0]);
+            int numeroDePeca1 = int.Parse(peca1[1]);
+            double valorDaPeca1 = double.Parse(peca1[2], CultureInfo.InvariantCulture);
+
+            string[] peca2 = Console.ReadLine().Split(" ");
+            int codigoPeca2 = int.Parse(peca2[0]);
+            int numeroDePeca2 = int.Parse(peca2[1]);
+            double valorDaPeca2 = double.Parse(peca2[2], CultureInfo.InvariantCulture);
+            
+            double valorAPagar = (numeroDePeca1 * valorDaPeca1) + (numeroDePeca2*valorDaPeca2);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {valorAPagar.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
