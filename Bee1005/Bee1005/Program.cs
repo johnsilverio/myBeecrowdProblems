2a599c9ccaa1f7f38c89a959dec8113958480d25
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 15:55:50 -0300

    Resolvido: problema 1005

---
+++ Bee1005/Bee1005/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee1005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pesoA = 3.5;
+            double pesoB = 7.5;
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double media = ((A * pesoA) + (B * pesoB)) / (pesoA + pesoB);
+            
+            Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
