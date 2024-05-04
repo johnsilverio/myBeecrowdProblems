3ba48bd9f0b0c8263b0046612ea00119cef96be0
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 18:43:07 -0300

    Resolvido: problema 1013 abs

---
+++ Bee1013/Bee1013/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bee1013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(" ");
+            int a, b, c;
+            a = int.Parse(valores[0]);
+            b = int.Parse(valores[1]);
+            c = int.Parse(valores[2]);
+            int maiorAB = ((a + b) + Math.Abs(a - b)) / 2;
+            int maior = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+            Console.WriteLine($"{maior} eh o maior");
+        }
+    }
+}
