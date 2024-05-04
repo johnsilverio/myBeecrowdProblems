b252ab3a6027509a56fed8591387f8a037aa0fbc
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 17:37:21 -0300

    Resolvido: problema 1009 strings

---
+++ Bee1009/String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double vendasEmDinheiro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorComissao = (vendasEmDinheiro * 0.15) + salario;
+            Console.WriteLine($"TOTAL = R$ {valorComissao.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
