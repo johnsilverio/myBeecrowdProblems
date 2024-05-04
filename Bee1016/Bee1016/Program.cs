cf9a8c3023bdcd1fdb6d3155c091f3205d266418
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 01:16:52 -0300

    Resolvido: problema 1016

---
+++ Bee1016/Bee1016/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bee1016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaKm = int.Parse(Console.ReadLine());
+            distanciaKm *= 2;
+            Console.WriteLine($"{distanciaKm} minutos");
+        }
+    }
+}
