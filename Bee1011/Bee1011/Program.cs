cde87d69e016f1c94be59db7f12bbebb181a4178
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 17:56:05 -0300

    Resolvido: problema 1011 Casting

---
+++ Bee1011/Bee1011/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = double.Parse(Console.ReadLine());
+            double volume = (4 / 3.0) * 3.14159 * Math.Pow(R, 3);
+            Console.WriteLine($"VOLUME = {volume.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
