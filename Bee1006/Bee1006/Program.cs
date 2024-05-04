e5314f7c915df7d01183bf600c6512163fcd9eb0
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 16:14:26 -0300

    Resolvido: problema 1006

---
+++ Bee1006/Bee1006/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee1006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pesoA, pesoB, pesoC;
+            pesoA = 2;
+            pesoB = 3;
+            pesoC = 5;
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double media = (((A * pesoA) + (B * pesoB) + (C * pesoC)) / (pesoA + pesoB + pesoC));
+            
+            Console.WriteLine($"MEDIA = {media.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
