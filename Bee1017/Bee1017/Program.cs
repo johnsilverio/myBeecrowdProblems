164adf7e75d121d2a4c255bfc7884b6482b84879
johnsilverio <johnvitor2002@hotmail.com>
2024-05-04 15:11:31 -0300

    Resolvido: problema 1017

---
+++ Bee1017/Bee1017/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee1017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoGastoEmHoras = int.Parse(Console.ReadLine());
+            int velociadeMedia = int.Parse(Console.ReadLine());
+            double litrosGastros = (tempoGastoEmHoras * velociadeMedia) / 12.00;
+            
+            Console.WriteLine(litrosGastros.ToString("F3"), CultureInfo.InvariantCulture);
+        }
+    }
+}
