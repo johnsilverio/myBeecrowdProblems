f53408e4b93d35b3e4c9f3149d488dff3e385cfb
johnsilverio <johnvitor2002@hotmail.com>
2024-05-06 13:51:03 -0300

    Resolvido: problema 1020

---
+++ Bee1020/Bee1020/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace  Bee1020
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            int dias = int.Parse(Console.ReadLine());
+
+            int idadeAno = dias / 365;
+            dias %= 365;
+            Console.WriteLine($"{idadeAno} ano(s)");
+            
+            int idadeMeses = dias / 30;
+            dias %= 30;
+            Console.WriteLine($"{idadeMeses} mes(es)");
+
+            int idadeDias = dias;
+            Console.WriteLine($"{idadeDias} dia(s)");
+        }
+    }
+}
