e1069cab0bb3b67c24646def333d6e215dce8898
johnsilverio <johnvitor2002@hotmail.com>
2024-05-08 14:24:39 -0300

    Resolvido: problema 1035

---
+++ Bee1035/Bee1035/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bee1035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+            string[] valores = Console.ReadLine().Split(' ');
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+            C = int.Parse(valores[2]);
+            D = int.Parse(valores[3]);
+
+            if (B > C && D > A && (C + D) > (A + B) && C >= 0 && D >= 0 && A % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
