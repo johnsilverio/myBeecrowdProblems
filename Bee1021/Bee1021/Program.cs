d34407d80f83a4c3692756e162357d99795525e4
johnsilverio <johnvitor2002@hotmail.com>
2024-05-08 14:03:56 -0300

    Resolvido: problema 1021

---
+++ Bee1021/Bee1021/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            
+            double valor = double.Parse(Console.ReadLine());
+
+            int[] notas = { 100, 50, 20, 10, 5, 2 };
+            double[] moedas = { 1, 0.50, 0.25, 0.10, 0.05, 0.01 };
+            
+            Console.WriteLine("NOTAS:");
+            foreach (int nota in notas)
+            {
+                int quantidadeNotas = (int)(valor / nota);
+                Console.WriteLine($"{quantidadeNotas} nota(s) de R$ {nota:F2}");
+                valor %= nota;
+                valor = Math.Round(valor, 2);
+            }
+
+            
+            Console.WriteLine("MOEDAS:");
+            foreach (double moeda in moedas)
+            {
+                int quantidadeMoedas = (int)(valor / moeda);
+                Console.WriteLine($"{quantidadeMoedas} moeda(s) de R$ {moeda:F2}");
+                valor %= moeda;
+                valor = Math.Round(valor, 2);
+            }
+        }
+    }
+}
