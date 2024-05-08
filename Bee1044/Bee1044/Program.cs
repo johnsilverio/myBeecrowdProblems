f4e80469e8c5b94a0c3954eaa3b213341abf8274
johnsilverio <johnvitor2002@hotmail.com>
2024-05-08 17:34:48 -0300

    Resolvido: problema 1044

---
+++ Bee1044/Bee1044/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bee1044
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split();
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
