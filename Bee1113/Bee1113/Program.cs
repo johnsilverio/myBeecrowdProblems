1434ad8ca8490d3aefa3b4f0f1d70fe2a2d7f37d
johnsilverio <johnvitor2002@hotmail.com>
2024-05-08 17:36:01 -0300

    Resolvido: problema 1113

---
+++ Bee1113/Bee1113/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bee1113
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+            string[] valores = Console.ReadLine().Split(' ');
+            X = int.Parse(valores[0]);
+            Y = int.Parse(valores[1]);
+
+            while (X != Y)
+            {
+                if (X < Y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                valores = Console.ReadLine().Split(' ');
+                X = int.Parse(valores[0]);
+                Y = int.Parse(valores[1]);
+            }
+        }
+    }
+}
